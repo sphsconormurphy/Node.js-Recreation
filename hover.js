c91a33ba393633894031f1af9d45e8edaafb4b9e
sphsconormurphy <8cmurphy@stpatrick.org>
2017-04-20 13:59:43 -0500

    Thursday 20 April

--- hover.js
+++ hover.js
@@ -1,1 +1,13 @@
+(function(){
+  const doc = document;
+  let div = doc.querySelectorAll("banner");
+  div.addEventListener("mouseover", function(){
+      div.style.border = 
+  });
+  div.addEventListener("mouseout", function(){
+      div.style.border =
+  });
 
+
+
+})();
