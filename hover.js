8a239da67aad1b22a8155b52fc187da429376a61
sphsconormurphy <8cmurphy@stpatrick.org>
2017-04-21 13:57:02 -0500

    Friday 21 April

--- hover.js
+++ hover.js
@@ -1,13 +1,23 @@
 (function(){
   const doc = document;
-  let div = doc.querySelectorAll("banner");
+  let div = doc.querySelector("banner");
   div.addEventListener("mouseover", function(){
-      div.style.border = 
+      div.style.border="10px solid green";
   });
   div.addEventListener("mouseout", function(){
-      div.style.border =
+      div.style.border ="10 px solid transparent";
   });
 
+})();
 
+(function(){
+  const doc = document;
+  let div = doc.querySelector("bottom div");
+  div.addEventListener("mouseover", function(){
+      div.style.backgroundColor="green";
+  });
+  div.addEventListener("mouseout", function(){
+      div.style.backgroundColor ="transparent";
+  });
 
 })();
